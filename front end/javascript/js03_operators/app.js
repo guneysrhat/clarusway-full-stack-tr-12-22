32fbd78364369cb2acae6b0027a6aad90583ecad
Serhat Guney <guneysrhat@gamil.com>
2022-08-13 11:22:45 +0300

    js lesson

---
+++ front end/javascript/js03_operators/app.js
@@ -0,0 +1,24 @@
+console.log('app js running');
+
+/*
+1
+
+*/
+
+// let num1 = + pr
+
+
+// let side1 = +prompt('enter first side');
+// let side2 = +prompt('enter second side');
+
+// let area = (side1 * side2);
+
+// console.log(`area of rectengle is ${area}`);
+
+
+
+const now = new Date();
+
+console.log(`Day of the month is ${now.getDate()}`);
+console.log(`Month is ${now.getMonth() + 1}`);
+console.log(`Year is ${now.getFullYear()}`);
