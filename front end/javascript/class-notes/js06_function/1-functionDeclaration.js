d8f05d47ca57f77c68f5072e1883f064da865e33
guneysrhat <guneysrhat@gmail.com>
2022-08-20 12:50:57 +0300

    function ex3

--- front end/javascript/class-notes/js06_function/1-functionDeclaration.js
+++ front end/javascript/class-notes/js06_function/1-functionDeclaration.js
@@ -31,3 +31,27 @@ function selamla(ad, soyad = "") {
 selamla("Can", "Yilmaz");
 selamla("Canan", "Ozturk");
 selamla("Ayse");
+
+//! Bir parametreyi cagirma sirasinda kullanmaz isek onun
+//! yerine default parametre atayabiliriz. Ornekteki lastName
+//! parametresi icin default değer olarak '' atanmistir.
+
+//* ORNEK 3:
+//******************************************************/
+
+function yasHesaplama(isim, dogumTarihi) {
+  //   const sonuc = `${isim} in yasi ${2022 - dogumTarihi} dir.`;
+  //   const sonuc = `${isim} in yasi ${
+  //     new Date().getFullYear() - dogumTarihi
+  //   } dir.`;
+  //   return sonuc;
+  return `${isim} in yasi ${new Date().getFullYear() - dogumTarihi} dir.`;
+}
+
+const mesaj1 = yasHesaplama("Elif Can", 1990);
+
+console.log(mesaj1);
+console.log(yasHesaplama("Veli Canan", 1980));
+
+//* ORNEK 3:
+//******************************************************/
