fe27ed692e4fb4a0bd09d210a1ff17b19da12df0
guneysrhat <guneysrhat@gmail.com>
2022-08-22 23:07:50 +0300

    scope

--- front end/javascript/class-notes/js07_function-2/3-scope.js
+++ front end/javascript/class-notes/js07_function-2/3-scope.js
@@ -2,3 +2,29 @@
 // ?                FONKSİYONLAR-SCOPE
 // ? ====================================================
 console.log("******* 3 - SCOPE ********");
+
+let sayi1 = 5; //! Glabal Scope
+console.log(sayi1); //! 5
+
+const fonks1 = function () {
+  sayi1 = 22;
+  console.log(sayi1); //! 22
+};
+fonks1();
+
+console.log(++sayi1); //! 23
+console.log(sayi1); //! 23
+console.log(sayi1++); //! 23
+console.log(sayi1); //! 24
+
+//?-----------------------------------------------------------
+let sayi2 = 7; //! Glabal Scope
+console.log(sayi2); //! 7
+
+const fonks2 = () => {
+  let sayi2 = 11; //! Function Scope
+  console.log(sayi2); //! 11
+};
+
+fonks2(); //! 11
+console.log(sayi2++); //!7
