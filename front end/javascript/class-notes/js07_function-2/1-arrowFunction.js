9295332000973d6baae32a76224a99e9ba50109c
guneysrhat <guneysrhat@gmail.com>
2022-08-22 20:43:40 +0300

    silindir hacmi

--- front end/javascript/class-notes/js07_function-2/1-arrowFunction.js
+++ front end/javascript/class-notes/js07_function-2/1-arrowFunction.js
@@ -10,3 +10,22 @@ console.log("**** ARROW ******");
 
 //* ORNEK:
 //************************************************/
+
+const topla = (a, b) => a + b;
+console.log(topla(2, 3));
+
+const usal = (t, u) => t ** u;
+console.log(usal(2, 3));
+
+const yazdir = () => console.log("Hi");
+const selam = (x) => console.log(x);
+
+//* ORNEK: Silindir Hacmi
+//************************************************/
+
+const r = Number(prompt("yaricap giriniz"));
+const h = Number(prompt("yukleklik giriniz"));
+
+const hacimHesapla = (r, h) => Math.PI * r ** 2 * h;
+
+console.log(`${r}, ${h} => Hacmi: ${hacimHesapla(r, h).toFixed(2)}`);
