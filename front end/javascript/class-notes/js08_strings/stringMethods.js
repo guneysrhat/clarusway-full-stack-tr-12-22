2709f082d0d989863a09bf3712dd1a1c92bcd084
guneysrhat <guneysrhat@gmail.com>
2022-08-24 20:07:07 +0300

    string

---
+++ front end/javascript/class-notes/js08_strings/stringMethods.js
@@ -0,0 +1,6 @@
+// ?======================================================
+// ?                     STRING METOTLARI
+// ! KAYNAK: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/String
+// ?======================================================
+
+console.log("*** STRING METHODS ***");
