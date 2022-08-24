b7e799c94ac71a383d89b748d985930ab9070fd3
guneysrhat <guneysrhat@gmail.com>
2022-08-24 22:50:27 +0300

    replace

--- front end/javascript/class-notes/js08_strings/stringMethods.js
+++ front end/javascript/class-notes/js08_strings/stringMethods.js
@@ -91,5 +91,80 @@ console.log(a.localeCompare(b, "en", { sensitivity: "base" }));
 //? 'CASE: a ≠ b, a = á, a ≠ A'
 
 //* ----------------------------------------------------------
-//* LocaleCompare()
+//* charAt()
 //* ----------------------------------------------------------
+const str6 = "Full Stack Path and DS path";
+console.log(str6.charAt(5)); //* S
+console.log(str6.charAt(4)); //* " "
+console.log(str6.charAt()); //* F (ilk indistekini dondurur)
+console.log(str6.charAt(14)); //* h
+
+//! lenght bir property (objenin degiskenidir) dir ve string'in
+//! karakter sayisini tutar.
+console.log(str6.length); //* 27
+
+//* String son harfini ogrenmek istersek
+console.log(str6.charAt(str6.length - 1)); //* 0 -26 => 27 eleman
+
+//* ----------------------------------------------------------
+//* includes() - case sentive
+//* ----------------------------------------------------------
+
+const word = "To be or not to be, that is the questions.";
+
+console.log(word.includes("to be")); //? true
+console.log(word.includes("That")); //? false
+console.log(word.includes("")); //? true
+console.log(word.includes("to be", 14)); //? false
+console.log(word.includes("to be", 13)); //? true
+
+//! NOT: case insentive kullanmak icin str baslangicta kucuk veya
+//! buyuk harfe cevirilerek arama yapilabilir.
+
+//* ----------------------------------------------------------
+//* indexOf() , lastIndexOf() - case sensitive
+//* ----------------------------------------------------------
+
+const toBe = word.indexOf("or");
+console.log(toBe); //? 6
+console.log(word.indexOf("be")); //? 3
+console.log(word.lastIndexOf("be")); //? 16
+console.log(word.lastIndexOf("BE")); //? -1
+
+//* ----------------------------------------------------------
+//* search()
+//* ----------------------------------------------------------
+
+const buyukHarf = /[A-Z]/;
+const nokta = /[.]/;
+const virgul = /[,]/;
+console.log(word.search(buyukHarf)); //? ilk buyuk harfin indeksi 0
+console.log(word.search(virgul)); //?  18
+console.log(word.search(nokta)); //? -1
+
+//* ----------------------------------------------------------
+//* startsWidth() , endsWith() - case sensitive
+//* ----------------------------------------------------------
+let word2 = "Sen gulunce guller acar gul pembe!";
+
+console.log(new String(word2));
+console.log(word2.startsWith("Sen")); //? true
+console.log(word2.startsWith("gulunce")); //? false
+console.log(word2.startsWith("gulunce", 4)); //? true
+console.log(word2.endsWith("!")); //? true
+console.log(word2.endsWith("r", 23)); //? true
+
+//* ----------------------------------------------------------
+//*  replace(searchFor, replaceWith) --immutable
+//* ----------------------------------------------------------
+let oku = "Oku Baban gibi, saf olma, saf olma";
+// console.log(oku.replace("saf olma", "basarili ol"));
+// oku = oku.replace("saf olma", "basarili ol");
+oku = oku.replace(/saf olma/gi, "basarili ol");
+
+console.log(oku);
+
+oku = oku.replace(/BASARILI/gi, "Zengin");
+console.log(oku);
+
+//! NOT: replace metodu eger Regex ile aksi belirtilmiyorsa (g) sadece ilk buldugunu degistirir.
