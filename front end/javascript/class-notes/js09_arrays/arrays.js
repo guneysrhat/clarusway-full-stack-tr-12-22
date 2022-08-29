3208f335a8618907f77dc21148ba7ac939591e4f
guneysrhat <guneysrhat@gmail.com>
2022-08-29 21:24:23 +0300

    Uncaught

--- front end/javascript/class-notes/js09_arrays/arrays.js
+++ front end/javascript/class-notes/js09_arrays/arrays.js
@@ -46,5 +46,29 @@ console.log(isimler[isimler.length - 1])
 //?at\()
 console.log(isimler.at(-1))
 
-//! Diziiye veri yazma
- // const isimler = ["ahmet" , "ismet", "can", "canan","Mathew"];
+//!  Diziye veri yazma
+// const isimler = ["ahmet", "ismet", "can", "canan", "Matheww"];
+isimler[1] = "Saffet";
+console.log(isimler);
+
+//!Uncaught TypeError: Assignment to constant variable.
+// isimler = ["Ali", "Veli"];
+
+ //? numbers dizisinn son elemanin  degerini bir arttirin
+ console.log(numbers)
+ numbers[numbers.length -1]++
+console.log(numbers)
+
+const yaslar = [22, 44, 55]
+const kisiler = ["ahmet", "can", 2022, 2022 - 1990 , true, yaslar]
+console.log(kisiler)
+
+//? kisiler icerisindeki yaslar dizisindebuklunan  55 yasini konsola yazdiriniz
+
+const yas55 = kisiler [5][2]
+console.log(yas55)
+
+//? kisiler icerisindeki yaslar dizinnde bulunan 44 yasini bir eksiltiniz
+kisiler [5][1]-- ; //? 43
+console.log(kisiler)
+console.log(yaslar)
