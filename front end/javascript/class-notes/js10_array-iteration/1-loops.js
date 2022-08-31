997e2f070a270089c074be73f901bc14b8711bf5
guneysrhat <guneysrhat@gmail.com>
2022-08-31 20:07:27 +0300

    starting lesoon

---
+++ front end/javascript/class-notes/js10_array-iteration/1-loops.js
@@ -0,0 +1,32 @@
+//* =====================================================
+//*                     FOR LOOP
+//* ======================================================
+
+console.log("******* LOOPS IN ARRAYS ******");
+
+
+
+
+
+
+
+
+
+//* ======================================================
+//*                   FOR-IN LOOP
+//* ======================================================
+
+
+
+
+
+
+
+
+//* ======================================================
+//*                   FOR-OF LOOP
+//* ======================================================
+
+//* for of dongusu, for in dongusunun bir cok veri yapisini
+//* kapsayacak sekilde guncellenmis halidir. Bu dongude dizi
+//* icersindeki veriye erisirken indisleme kullanmaya gerek yoktur.
---
+++ front end/javascript/codding-class/ch-12 2/app.js
@@ -0,0 +1,61 @@
+1. Bölüm Loops
+
+//! Q-1
+//? Ask the user to input a number. Check the input of the user, if it is not a valid number
+//? then keep getting input from the user until provides a valid number.
+
+
+//! Q-2
+//? Write a JavaScript program to compute the greatest common divisor (GCD) (OBEB) of two positive integers.
+
+
+//! Q-3
+//? Write a code that displays the factors (tam bolen) of a number entered by the user.
+
+//! Q-4
+//? Write a code to calculate the factorial of a number entered by the user.
+
+//! Q-5
+//? Write a code to find numbers that are divisible by 3 and 5, between 100 and 1000.
+
+
+//! Q-6
+//? Write a code that prints a multiplication table of numbers 1 to 10.
+
+
+//! Q-7
+//? Write a code to draw Whiteball and Powerball numbers for the Powerball lottery game. 
+//? 5 Whiteball numbers are chosen from 01 to 69 and 1 Powerball number is chosen from 1 to 26.
+
+
+
+2. Bölüm Functions
+
+//! Q-1
+//? Write a Function that takes 2 parameters (amount and rate) to convert currency.
+
+//! Q-2
+//? Write a JavaScript function that accepts an argument and returns the type.
+
+//! Q-3
+//? You are given a string and character. Write a function to check the string and find how many
+//? times this character occurs in this string.
+//? Example:
+//? str = "Clarusway"         char = "a"       ===> result = 2 as "a" occurs 2 times in Clarusway
+
+//! Q-4
+//? Write a JS code to display Perfect Numbers from 1 to 1000.
+//? A perfect number is a positive integer that is equal to the sum of its positive divisors, excluding the number itself.
+//? For instance, 6 has divisors 1, 2 and 3 (excluding itself), and 1 + 2 + 3 = 6, so 6 is a perfect number.
+
+
+//! Q-5
+//? Develop a guessing game. Randomly generate a number between 0-100. Ask the user to guess that number.
+//? The user should have 5 chance to guess the number.
+//? If the number entered by the user:
+//? is greater than the random number then warn user to decrease,
+//? is less than the random number then warn user to increase,
+//? is equal to the random number then congratulate the user and tell in how many guesses did the user find the number
+//? If user can't find the number in 5 guess then game is lost and display the random number.
+//? Hint: you can use this code to generate random number 0-100
+//? const winingNumber = Math.round(Math.random() * 100);
