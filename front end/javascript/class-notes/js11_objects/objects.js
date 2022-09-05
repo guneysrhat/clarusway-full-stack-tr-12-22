afa6095ab924bac6d488239bce411ce01281ce7f
guneysrhat <guneysrhat@gmail.com>
2022-09-05 20:31:27 +0300

    new

---
+++ front end/javascript/class-notes/js11_objects/objects.js
@@ -0,0 +1,12 @@
+//* ===================================================
+//*                 (OBJECTS)
+//* ===================================================
+
+//? Diziler sirali bellek bölgeleridir.Bu yuzden dizilere
+//? indeksleme ile erisebiliriz. Ancak daha karmasik (unstructured)
+//? verilerimiz icin  Object'leri kullanabiliriz.
+
+//? Object'lerde Key-value (property-value) yapisi kullanilir.
+//? Herhangi bir veriyi erismek icin property (key) adi kullanilir.
+
+console.log("******* Objects *********");
