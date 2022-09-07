cb51591cbf5a6d2290087bc300b78c6a896a5a3b
guneysrhat <guneysrhat@gmail.com>
2022-09-07 23:40:28 +0300

    last

--- front end/javascript/class-notes/js12_new-gen-operators/newOps.js
+++ front end/javascript/class-notes/js12_new-gen-operators/newOps.js
@@ -154,7 +154,13 @@ const sumAll = (...numbers) => {
 
 console.log("SUM OF NUMBERS:", sumAll(1, 2, 3, 4, 5, 6));
 
+const showName = (name, surname, ...titles) => {
+  console.log(titles);
+  const summary = `${name} ${surname} is a ${titles.join(" and ")}`;
+  console.log(summary);
+};
 
+showName(`Noah`, `Adams`, `Developer`, `Instr`, `Professor`, `Dad`);
 
 //*======================================================
 //*  SPREAD (...)
