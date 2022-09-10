11d8caa73e545facb1d4d8d14e51d7d35d4a86ef
guneysrhat <guneysrhat@gmail.com>
2022-09-10 12:34:13 +0300

    GETELEMENTSBYTAGNAME

--- front end/javascript/class-notes/js13_dom-intro/1-selectors.js
+++ front end/javascript/class-notes/js13_dom-intro/1-selectors.js
@@ -6,3 +6,53 @@ console.log("***** SELECTORS *****");
 
 //* EXAMPLE-1
 //*-------------------------------------------
+
+const h1 = document.getElementById("header");
+console.log(h1);
+h1.style.color = "white";
+h1.style.backgroundColor = "red";
+
+//* EXAMPLE-2
+//*-------------------------------------------
+
+const myInput = document.getElementById("input");
+const button = document.getElementById("btn");
+button.style.background = "black";
+button.style.color = "white";
+button.style.width = "7rem";
+button.style.border = "none";
+button.style.borderRadius = "10px";
+button.style.padding = "1rem";
+button.style.fontSize = "1.1rem";
+myInput.style.padding = "1rem";
+
+//*===========================================
+//*          GETELEMENTSBYTAGNAME()
+//*===========================================
+
+//*EXAMPLE-3
+//*-------------------------------------------
+
+const list = document.getElementsByTagName("li");
+console.log(list); //? HTML Collection
+list[2].style.color = "red";
+const elementThree = list.item(3);
+elementThree.style.color = "blue";
+elementThree.textContent = "React / Vue / Angular";
+
+list[4].innerText = "Django / Flask";
+
+console.log(list[1].textContent);
+console.log(list[1].innerText);
+
+
+//*===========================================
+//*          GETELEMENTSBYCLASSNAME()
+//*===========================================
+
+//* EXAMPLE-4
+//* ------------------------------------------
+
+//* ========================================
+//*              QUERYSELECTOR()
+//* ========================================
