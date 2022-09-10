14e9bdfa3f61fb136419d31df4344f7b22b1412a
guneysrhat <guneysrhat@gmail.com>
2022-09-10 14:45:37 +0300

    INNER HTML

--- front end/javascript/class-notes/js13_dom-intro/3-createNode.js
+++ front end/javascript/class-notes/js13_dom-intro/3-createNode.js
@@ -4,9 +4,24 @@ console.log("**** CREATE NODE ****");
 const newP = document.createElement("p");
 newP.id = "new-par";
 newP.setAttribute("class", "new-class");
-const text = document.createTextNode("this is a new p element");
+const text = document.createTextNode("This is a new p element");
 newP.appendChild(text);
 console.log(newP);
 
 const h2 = document.querySelector(".item-list h2");
 h2.after(newP);
+
+//? getAttribute()
+console.log(newP.getAttribute("class"));
+console.log(newP.getAttribute("id"));
+console.log(newP.getAttribute("name"));
+
+//? classList
+console.log(newP.classList.contains("par")); //? true
+newP.classList.add("new-class");
+console.log(newP);
+newP.classList.remove("new-class");
+
+//? INNER HTML (Ciddi Guvenlik aciklari var)
+const ul = document.querySelector("ul");
+ul.innerHTML += `<li id="list-id" class="list">Boostrap</li>`;
