39a17701463f2f448f16f2b6c0cbd52e02a4a42a
guneysrhat <guneysrhat@gmail.com>
2022-09-12 21:00:34 +0300

    addEventListener

--- front end/javascript/class-notes/js14_events/1-events-intro/events.js
+++ front end/javascript/class-notes/js14_events/1-events-intro/events.js
@@ -6,10 +6,36 @@ console.log("****** EVENTS *******");
 
 //* EXAMPLE-1 (Mouse Over, Mouse Out )
 //* -------------------------------------------------------
+//? 3. Method
+const h1 = document.querySelector("#header h1");
+h1.onmouseover = function () {
+  h1.style.color = "red";
+  h1.style.fontWeight = "300";
+};
+h1.onmouseout = () => {
+  h1.style.color = "black";
+  h1.style.fontWeight = "900";
+};
 
-
-
-
+//? 4.Method (addElementListener)
+h1.addEventListener("click", () => {
+  alert("h1 pressed");
+});
 
 //* EXAMPLE-2 (addEventListener())
 //* -------------------------------------------------
+
+document.getElementById("btn").addEventListener("click", function () {
+  const input = document.querySelector("#input");
+  //   if (!input.value) {
+  //     alert("Please enter an items");
+  //   } else {
+  //     alert(`${input.value} entered`);
+  //   }
+
+  !input.value
+    ? alert("Please enter an items")
+    : alert(`${input.value} entered`);
+
+  input.value = "";
+});
