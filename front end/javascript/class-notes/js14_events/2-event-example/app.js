169aa0e8c22fa5898a11b2d5c6a663794177764d
guneysrhat <guneysrhat@gmail.com>
2022-09-12 22:41:09 +0300

    add

--- front end/javascript/class-notes/js14_events/2-event-example/app.js
+++ front end/javascript/class-notes/js14_events/2-event-example/app.js
@@ -1,1 +1,25 @@
 console.log("**** app.js *******");
+
+//? Selectors
+const langInput = document.querySelector(".lang-input");
+const addBtn = document.querySelector("#add-btn");
+const deleteBtn = document.querySelector("#delete-btn");
+const langList = document.getElementById("lang-list");
+
+const newUl = document.createElement("ul");
+langList.appendChild(newUl);
+
+//? addBtn event handler
+addBtn.addEventListener("click", () => {
+  if (!langInput.value) {
+    alert("Please enter a language");
+  } else {
+    newUl.innerHTML += `<li>${langInput.value}</li>`;
+    langInput.value = "";
+  }
+  langInput.focus();
+});
+
+window.addEventListener("load", () => {
+  langInput.focus();
+});
