730ea9b04ed8c6c3d1d7a957312c4f4d8f4ab516
guneysrhat <guneysrhat@gmail.com>
2022-09-22 21:32:26 +0300

    Callback Hell

--- front end/javascript/class-notes/js18_async1/1-timeout_Interval.js
+++ front end/javascript/class-notes/js18_async1/1-timeout_Interval.js
@@ -35,9 +35,59 @@
 
 //* Asenkron (setTimeout)
 //* ------------------------------------------------
-console.log("Timeout");
+
+// console.log("Timeout");
+// setTimeout(() => {
+//   //! non-blocking
+//   console.log("Hi");
+// }, 1000);
+// console.log("timeout bitti");
+
+// console.log("Timeout");
+// setTimeout(() => {
+//   //! non-blocking
+//   console.log("Hello");
+// }, 3000);
+// console.log("timeout bitti");
+
+//* Asenkron (setInterval, clearInterval)
+//* ------------------------------------------------
+
+console.log("timer started");
+let counter = 0;
+const intervalId = setInterval(() => {
+  console.log(++counter);
+  if (counter > 4) {
+    clearInterval(intervalId);
+  }
+}, 1000);
+
+console.log("timer stoped");
+
+
+//! Callback Hell (nested ve birbirine bagli callback'ler)
+//!-----------------------------------------------------
+//* Eger birbirine bagimli asenkron kodlarin yazilmasi gerekirse,nested callback
+//* yapisinin kullanilmasi gerekebilir. Fakat bu iyi bir programlama yaklasimi degildir.
+// !callback hell olarak adlandirilan bu yapinin anlasilmasi ve surdurulebilirligi oldukca zordur.
+
 setTimeout(() => {
-  //! non-blocking
-  console.log("Hi");
-}, 2000);
-console.log("timeout bitti");
+    console.log("john:Hi");
+    setTimeout(() => {
+      console.log("Sarah: Hello");
+      setTimeout(() => {
+        console.log("John: How Are you?");
+        setTimeout(() => {
+          console.log("Sarah:Fine and you?");
+        }, 1000);
+      }, 1000);
+    }, 1000);
+  }, 1000);
+  
+  //? COZUMLER:
+  //?----------------------------------------------------
+  //* 1- XMLHttpRequest (Eski yontem, Ornek: AJAX)
+  //? https://developer.mozilla.org/en-US/docs/Web/API/XMLHttpRequest
+  //* 2- Promise,
+  //! 3- Fetch API (Promise'in basitlestirilmis hali),
+  //! 4- ASYNC-AWAIT (Fetch API'nin makyajlanmis hali)
