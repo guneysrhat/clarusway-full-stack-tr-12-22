250b0481311c4242d72ae9a14a6070024a821745
guneysrhat <guneysrhat@gmail.com>
2022-09-24 13:17:46 +0300

    error message

--- front end/javascript/class-notes/js19_async2/async-await/async-await.js
+++ front end/javascript/class-notes/js19_async2/async-await/async-await.js
@@ -17,7 +17,7 @@
 //* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
 //* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
 //* degerlerinin dondurulmesine ile kodun calismasi devam eder.
-
+let isError = false;
 const getNews = async function () {
   const API_KEY = "c0180d3f34234b9a8e58dca47ef87c47";
 
@@ -27,7 +27,8 @@ const getNews = async function () {
   try {
     const res = await fetch(url);
     if (!res.ok) {
-      throw new error(`Someting went wrong ${res.status} `);
+      isError = true;
+      //   throw new Error(`Someting went wrong ${res.status} `);
     }
     const data = await res.json();
     console.log(data.articles);
@@ -38,7 +39,34 @@ const getNews = async function () {
 };
 
 const renderNews = (news) => {
-    
+  const newList = document.getElementById("news-list");
+  if (isError) {
+    newList.innerHTML += `
+    <div class="alert alert-danger" role="alert">
+    News can not be fatched!!
+        </div>
+   
+    <img src="./img/404.png"/>
+    `;
+
+    return;
+  }
+
+  news.forEach((item) => {
+    const { title, description, urlToImage, url } = item; //! dest
+    newList.innerHTML += `
+    <div class=" col-md-6 col-lg-4 col-xl-3 ">
+        <div class="card" style="width: 18rem;">
+            <img src="${urlToImage}" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${title}</h5>
+                    <p class="card-text">${description}</p>
+                    <a href="${url}" target="_blank" class="btn btn-danger">Details</a>
+                </div>
+        </div>
+    </div>
+   `;
+  });
 };
 
 window.addEventListener("load", getNews);
