075bb422d742e392ac1831e6d93f2a133599051a
guneysrhat <guneysrhat@gmail.com>
2022-09-24 11:54:19 +0300

    start

---
+++ front end/javascript/class-notes/js19_async2/async-await/async-await.js
@@ -0,0 +1,19 @@
+//*=================================================
+//*              ASYNC-AWAIT
+//*=================================================
+//? Async-Await ECMAScript 2017 ile Javascript diline eklenmistir.
+//? Aslinda Promise yapisinin syntax olarak basitlestirilmis halidir.
+//? Bu baglamda sentetik seker benzetmesi yapilabilir.
+
+//* Bir fonskiyonu asenkron hale getirmek icin fonksiyon keyword'nun onune
+//* async keyword'u eklenir.
+
+//* Bir async fonksiyon icerisinde await keyword'u ile yapilan istegin cevabinin
+//* beklenmesi saglanir.
+
+//* Aslinda dizilis olarak senkron mantiga benzeyen kod yazarak Asenkron
+//* kod yazmayı mumkun kilar.
+
+//* Await, promise-temelli herhangi bir fonksiyonun onune getirilerek getirildigi
+//* satirdaki kodun durudurulmasini saglar. Yapilan istek yerine getirilip sonuc
+//* degerlerinin dondurulmesine ile kodun calismasi devam eder.
---
+++ front end/javascript/class-notes/js19_async2/async-await/readme.md
@@ -0,0 +1,5 @@
+## Asyn-await ornegimiz icin aşağıdaki API'en faydalancağız.
+
+## Bu sebeple, belirtilen linke giderek "GET API Key" butonuna tıklayalım ve kısaaca bilgilerimizi girerek bir API KEY alalım.
+
+https://newsapi.org/
