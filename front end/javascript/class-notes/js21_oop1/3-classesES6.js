1cd55a6ba7b6fc6512ac3f1bebb50079d4acc27f
guneysrhat <guneysrhat@gmail.com>
2022-09-29 22:28:09 +0300

    Sub-Class

--- front end/javascript/class-notes/js21_oop1/3-classesES6.js
+++ front end/javascript/class-notes/js21_oop1/3-classesES6.js
@@ -11,3 +11,38 @@
 
 //? Bir parent class'in degisken ve fonksiyonelliği extends
 //? keyword'u ile child class'a gecmektedir.(INHERITANCE)
+
+class Book {
+  constructor(title, author, year) {
+    this.author = author;
+    this.title = title;
+    this.year = year;
+
+    //? bu alana yazilan bur metot butun instance`larin belleginde tek tek yer kaplar
+    this.getTitle = function () {
+      return this.title;
+    };
+  }
+
+  //! bu kisimda yazilan fonksiyonlar aslinda prototype alaninda bulunur
+  getAge() {
+    return new Date().getFullYear() - this.year;
+  }
+
+  getSummary() {
+    return `${this.title} was writtten by ${this.author} in ${this.year}`;
+  }
+}
+
+
+
+//?instance
+const book2 = new Book("Sinekli Bakkal", "H. Edip Adıvar", 1930);
+
+console.log(book1);
+console.log(book1.getAge());
+console.log(book1.getSummary());
+
+//? Sub-Class tanimlamasi (Inheritance)
+
+class
