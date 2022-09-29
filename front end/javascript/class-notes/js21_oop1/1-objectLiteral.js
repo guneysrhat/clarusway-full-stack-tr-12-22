37e15575613a89aa751dcc61676e0065af4635dd
guneysrhat <guneysrhat@gmail.com>
2022-09-29 20:23:38 +0300

    get summary

--- front end/javascript/class-notes/js21_oop1/1-objectLiteral.js
+++ front end/javascript/class-notes/js21_oop1/1-objectLiteral.js
@@ -14,5 +14,21 @@ const book1 = {
   },
 };
 
-
 console.log(book1);
+console.log(book1.hasOwnProperty("author")); //?true
+console.log(book1.getSummary());
+//! object nesnesinden miras yoluyla geldi.
+
+const book2 = {
+  title: "The Lily of the valley",
+  author: "Honero de Balzac",
+  year: 1888,
+  getSummary: function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+};
+
+console.log(book2);
+console.log(book2.getSummary());
+
+
