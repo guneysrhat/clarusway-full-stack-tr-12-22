8386c708d59ede66e9f80d003fecda109de62f88
guneysrhat <guneysrhat@gmail.com>
2022-09-29 20:18:30 +0300

    create obj

--- front end/javascript/class-notes/js21_oop1/1-objectLiteral.js
+++ front end/javascript/class-notes/js21_oop1/1-objectLiteral.js
@@ -4,3 +4,15 @@
 
 //* Object Literals
 console.log("**** Object Literals ****");
+
+const book1 = {
+  title: "The Kramazov Brothers",
+  author: "Dostoevski",
+  year: 1880,
+  getSummary: function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  },
+};
+
+
+console.log(book1);
