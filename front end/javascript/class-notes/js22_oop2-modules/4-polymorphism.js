f2769aef61ef8f2a6b1ca1e26a577446fe960b49
guneysrhat <guneysrhat@gmail.com>
2022-10-01 11:30:51 +0300

    overloading

--- front end/javascript/class-notes/js22_oop2-modules/4-polymorphism.js
+++ front end/javascript/class-notes/js22_oop2-modules/4-polymorphism.js
@@ -26,6 +26,11 @@ class Book {
   getSummary() {
     return `${this.title} was writtten by ${this.author} in ${this.year}`;
   }
+
+  setPrice(price) {
+    const taxRate = 1.1;
+    this.price = Math.trunc(price * taxRate);
+  }
 }
 
 //? Sub-Class tanimlamasi (Inheritance)
@@ -36,7 +41,7 @@ class Magazine extends Book {
     this.month = month;
   }
   //! Overrided Metot (Parent class'daki bir metodun farkli
-  //! fonksiyonellikle fakat ayni isimle tanimlanmasi)
+  //! fonksiyonellikle  ve ayni parametre listesi ile yeniden tanimlanmasi)
   getSummary() {
     return `${this.title} was writtten by ${this.author} in ${this.year} in ${this.month}`;
   }
@@ -45,9 +50,37 @@ class Magazine extends Book {
   getSummaryParent() {
     return super.getSummary();
   }
+  //! Overloaded Metot (Ayni metodun farkli parametreler ile kullanilmasi)
+  setPrice(price, taxRate = 1.1) {
+    this.price = Math.trunc(price * taxRate);
+  }
 }
+
+//? Parent
+const book1 = new Book("Simyaci", "Poelho Coelgo", 1988);
+console.log(book1);
+book1.setPrice(100);
+console.log(book1);
 
 //? Magazine objesinin yeni bir instance
 const mag1 = new Magazine("Scientific Research", "Einstein", 1926, "Sep");
 console.log(mag1.getSummary());
 console.log(mag1.getSummaryParent());
+
+mag1.setPrice(50);
+console.log(mag1);
+
+//? JS'de overloading, parent-child class arasinda kullanilir.
+//? Ancak diger bir cok dilde ayni metot ayni class icerisinde de farkli paramtreler ile tekrar tekrar yazilabilir.
+//? overloading
+// function x(string a, string b) {
+//   return a + b;
+// }
+
+// //? overloading
+// function x(integer a, integer b) {
+//   return a + b;
+// }
+
+// x(1, 2);
+// x("1","2")
