2c52460d59093678c0c4141d8045c6ca19238ba0
guneysrhat <guneysrhat@gmail.com>
2022-10-01 13:05:56 +0300

    oop done

--- front end/javascript/class-notes/js22_oop2-modules/5-static-private.js
+++ front end/javascript/class-notes/js22_oop2-modules/5-static-private.js
@@ -24,47 +24,91 @@
 //! Private metotlara ancak class icerisindeki diger metotlar ile erisilebilir.
 
 class Book {
-  //? Private property
-  #id = "123456";
-  constructor(title, author, year) {
-    this.author = author;
-    this.title = title;
-    this.year = year;
-
-    this.getTitle = function () {
-      return this.title;
-    };
+    //!Private degisken tanimlamasi
+    #id;
+  
+    //! static property tanimlamasi
+    static counter = 0;
+  
+    constructor(title, author, year) {
+      this.author = author;
+      this.title = title;
+      this.year = year;
+  
+      //? Private property
+      this.#id = 44;
+      this.getTitle = function () {
+        return this.title;
+      };
+  
+      //* static degiskenin degerini degistirdik
+      Book.counter++; //! ClasName.propertyName
+    }
+  
+    //? Class icerisinde public metotlar yardimiyla private degiskenler okunabilir.
+    //? Bu tip metotlara getter metot denilir.
+    //! getter metotlari class icerisinde tanimlanmalidir.
+    getId() {
+      return this.#id;
+    }
+  
+    //? Class icerisinde public metotlar yardimiyla private degiskenlere yazilabilir.
+    //? Bu tip metotlara setter metot denilir.
+    //! setter metotlar class icerisinde tanimlanmalidir.
+    setId(id) {
+      this.#id = id;
+    }
+  
+    getSummary() {
+      return `${this.title} was writtten by ${
+        this.author
+      } so its age is ${this.#computeAge()} `;
+    }
+  
+    //!private metot tanimlanmsi
+    #computeAge() {
+      return new Date().getFullYear() - this.year;
+    }
+  
+    //? static method
+    static compareAge(b1, b2) {
+      return `Books age difference: ${b1.year - b2.year}`;
+    }
   }
-
-  //? Class icerisinde public metotlar yardimiyla private degiskenler okunabilir.
-  //? Bu tip metotlara getter metot denilir.
-  //! getter metotlari class icerisinde tanimlanmalidir.
-  getId() {
-    return this.#id;
-  }
-
-  //? Class icerisinde public metotlar yardimiyla private degiskenlere yazilabilir.
-  //? Bu tip metotlara setter metot denilir.
-  //! setter metotlar class icerisinde tanimlanmalidir.
-  setId(id) {
-    this.#id = id;
-  }
-}
-
-const book1 = new Book("Simyaci", "Poelho Coelgo", 1988);
-
-console.log(book1.title);
-
-//? Private bir degiskenin degeri class disindan dogrudan okunamaz.
-// console.log(book1.#id);
-
-//? Private bir degiskenin degeri class disindan dogrudan degistirilemez
-// book1.#id = "11111";
-
-//! Private field '#id' must be declared in an enclosing class (at 5-static-private.js:42:18)
-//? Private degiskeni okuma
-console.log(book1.getId());
-
-//? Private degiskene deger atama
-book1.setId("00000");
-console.log(book1.getId());
+  
+  const book1 = new Book("Simyaci", "Poelho Coelgo", 1988);
+  console.log(book1.title);
+  
+  //? Private bir degiskenin degeri class disindan dogrudan okunamaz.
+  // console.log(book1.#id);
+  
+  //? Private bir degiskenin degeri class disindan dogrudan degistirilemez
+  // book1.#id = "11111";
+  
+  //! Private field '#id' must be declared in an enclosing class (at 5-static-private.js:42:18)
+  //? Private degiskeni okuma
+  console.log(book1.getId());
+  
+  //? Private degiskene deger atama
+  book1.setId("00000");
+  console.log(book1.getId());
+  
+  //!Private metotlar class disirasindan erisilemezler.
+  //! Ancak class icerisindeki bir metottan erilebilrler.
+  // console.log(book1.#computeAge());
+  
+  console.log(book1.getSummary());
+  
+  const book2 = new Book("ABC", "Ali Veli", 1988);
+  const book3 = new Book("XYZ", "Ahmet Can", 1988);
+  const book4 = new Book("XYZ", "Ahmet Can", 1920);
+  
+  //! Static degiskenlere sadece class uzerinden erisilebilir
+  console.log(Book.counter);
+  
+  //! instance'lar üzerinden static degiskenlere erilemez.
+  console.log(book1.counter);
+  
+  //? Static metot cagrilmasi
+  console.log(Book.compareAge(book2, book4));
+  
