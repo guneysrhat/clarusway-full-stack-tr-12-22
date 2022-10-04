61fb8b53e816d986e03e4b9e8af1367b766f447b
guneysrhat <guneysrhat@gmail.com>
2022-10-04 21:22:51 +0300

    formJquery

--- front_end/javascript/class-notes/js24-jquery/weather.js
+++ front_end/javascript/class-notes/js24-jquery/weather.js
@@ -1,26 +1,50 @@
 const formJS = document.querySelectorAll("form")[0];
-//* JQUERY === $
-// const formJquery = jQuery("form").eq(0);
+//*jQUERY === $
+//const formJquery = jQuery("form").eq(0);
 const formJquery = $("form").eq(0);
 const inputJQ = $(".top-banner input").eq(0);
 const msgJQ = $(".top-banner span").eq(0);
 const listJQ = $(".cities").eq(0);
 
-// console.log(formJS);
-// console.log(formJquery);
-console.log(inputJQ);
+//*console.log(formJS);
+//*console.log(formJquery);
+//*console.log(inputJQ);
 
-//* get (index) ==> toArray(get()), eq(index),
+//* get(index) ==> toArray(get()) , eq(index)
 
-//* Load VS DOMContentLoaded
+//*load VS DOMContentLoaded
+//*DOMContentLoaded ==> means page rendered, DOM is ready
+//*window load ==> (all content (e.g. images, styles etc) also loaded)
 
-//* load vs DOMContentLoaded
-//* DOMContentLoaded ===> means page rendered, DOM is ready
-//* window load ==> (all content (e.g. images, styles etc.) also loaded)
-
-//* window.onload = () => {} ===> JS
+//*window.onload = () =>{} ===> JS
 //* addEventListener ===> on
 
 $(window).on("load", () => {
-  console.log(window.load);
+  console.log("window.load");
 });
+
+//* document.addEventListener("DOMContentLoaded", ()=>{}) ==> JS
+
+//* $(document).on("DOMContentLoaded", ()=>{
+//*     console.log("DOMContentLoaded");
+//* });
+
+$(document).ready(() => {
+  console.log("DOMContentLoaded");
+});
+
+//* formJquery.on("submit", (e)=>{
+//*     e.preventDefault();
+//*     getWeatherDataFromApi();
+//* });
+
+formJquery.submit((e) => {
+  e.preventDefault();
+  getWeatherDataFromApi();
+});
+
+const getWeatherDataFromApi = () => {
+  //*console.log("AJAX Func. is called");
+};
+
+//* XMLHTTPREQUEST(xhr) vs. fetch() vs. axios vs. $.ajax
