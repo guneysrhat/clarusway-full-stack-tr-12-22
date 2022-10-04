e3002ad14ef377e2860f3b250fedc5303c2a86ea
guneysrhat <guneysrhat@gmail.com>
2022-10-04 22:34:25 +0300

    error

--- front_end/javascript/class-notes/js24-jquery/weather.js
+++ front_end/javascript/class-notes/js24-jquery/weather.js
@@ -56,11 +56,76 @@ const getWeatherDataFromApi = () => {
   console.log(cityName);
   const units = "metric";
   const lang = "tr";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}&lang=${lang} `;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}&lang=${lang}`;
+
+  $.ajax({
+    type: "GET",
+    url: url,
+    dataType: "json",
+    success: (response) => {
+      //*main body func.
+      console.log(response);
+
+      const { main, sys, name, weather } = response;
+      const iconUrlAWS = `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0].icon}.svg`;
+      //alternative iconUrl
+      const iconUrl = `https://openweathermap.org/img/wn/${weather[0].icon}@2x.png`;
+
+      //*  /js=>document.createElement("li")
+
+      // const createdLi2 = $(document.createElement("li"))
+
+      //weather card control!!
+
+      const cityCardList = listJQ.find(".city");
+      const cityCardListArray = cityCardList.get();
+
+      console.log(cityCardList);
+      if (cityCardListArray.length > 0) {
+        const filteredArray = cityCardListArray.filter(
+          (li) => $(li).find("span").text() == name
+        );
+        if (filteredArray.length > 0) {
+          msgJQ.text(
+            `You already know the weather for ${name}, Please search for another city 😉`
+          );
+          msgJQ.css({ color: "red" });
+          return;
+        }
+      }
+
+      const createdLi = $("<li></li>");
+      createdLi.addClass("city");
+      createdLi.html(`
+      <h2 class="city-name" data-name="${name}, ${sys.country}">
+          <span>${name}</span>
+          <sup>${sys.country}</sup>
+      </h2>
+      <div class="city-temp">${Math.round(main.temp)}<sup>°C</sup></div>
+      <figure>
+          <img class="city-icon" src="${iconUrl}">
+          <figcaption>${weather[0].description}</figcaption>
+      </figure>`);
+
+      //* append vs. prepend both in JS AND JQUERY
+      listJQ.prepend(createdLi);
+      //   formJS.reset();
+      formJquery.trigger("reset");
+    },
+    beforeSend: (request) => {
+      console.log("before ajax send");
+    },
+    complete: () => {
+      console.log("after ajax send");
+    },
+    error: (XMLHttpRequest) => {
+        //* logging 
+      //console.log(" XMLHttpRequest ");
+      msgJQ.text(`${XMLHttpRequest.status} ${XMLHttpRequest.statusText}`);
+      msgJQ.css({ color: "red", "text-decorations": "underline" });
+      return;
+    },
+  });
 };
 
 //* XMLHTTPREQUEST(xhr) vs. fetch() vs. axios vs. $.ajax
-
-$.ajax({
-    type : 
-});
