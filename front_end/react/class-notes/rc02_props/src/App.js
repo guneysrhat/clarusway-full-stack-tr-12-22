cd13b66552bae515490c41d5a31dece9e65e5551
guneysrhat <guneysrhat@gmail.com>
2022-10-08 14:06:34 +0300

    finised

--- front_end/react/class-notes/rc02_props/src/App.js
+++ front_end/react/class-notes/rc02_props/src/App.js
@@ -6,9 +6,14 @@ import data from "./data";
 function App() {
   return (
     <div>
-      <Person name={data[0].name} img={data[0].img} tel={data[0].tel} />
-      <Person name={data[1].name} img={data[1].img} tel={data[1].tel} />
-      <Person name={data[2].name} img={data[2].img} tel={data[2].tel} />
+      {data.map((person) => (
+        <Person
+          key={person.name}
+          name={person.name}
+          img={person.img}
+          tel={person.tel}
+        />
+      ))}
     </div>
   );
 }
