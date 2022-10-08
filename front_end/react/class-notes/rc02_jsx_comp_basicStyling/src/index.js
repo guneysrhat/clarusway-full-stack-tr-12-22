2b5d162b62e85e3e8da4a29a89177be53e6d7e8f
guneysrhat <guneysrhat@gmail.com>
2022-10-08 11:48:18 +0300

    create element

--- front_end/react/class-notes/rc02_jsx_comp_basicStyling/src/index.js
+++ front_end/react/class-notes/rc02_jsx_comp_basicStyling/src/index.js
@@ -1,8 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-import App from "./App";
+// import App from "./App";
 import "./index.css";
+const a = 5;
+const element = (
+  <div>
+    <h1>Hello React {a} </h1>
+    <p>merhaba</p>
+    
+  </div>
+);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(element);
