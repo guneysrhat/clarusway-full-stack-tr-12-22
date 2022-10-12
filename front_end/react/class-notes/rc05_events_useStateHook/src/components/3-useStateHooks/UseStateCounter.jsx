14f7ef1adab58d2b42306a144c0b690a2ea600e8
guneysrhat <guneysrhat@gmail.com>
2022-10-12 23:05:25 +0300

    usestateCounter

--- front_end/react/class-notes/rc05_events_useStateHook/src/components/3-useStateHooks/UseStateCounter.jsx
+++ front_end/react/class-notes/rc05_events_useStateHook/src/components/3-useStateHooks/UseStateCounter.jsx
@@ -17,41 +17,77 @@
 //*    (Custom hook'lar icerisinde bir hook cagrilabilir)
 //?    https://reactjs.org/docs/hooks-rules.html
 //* =============================================================
+
 import { useState } from "react";
 
 const UseStateCounter = () => {
   //* useState en cok kullanilan Hook'tur.
   //* Bir state'in degisken, dizi ve obje ile kullanilabilmesine olanak saglar.
   //? useState hook'u bir dizi dondurur.Bu dizi array dest ile acilabilir.
-  //? Acilan dizinin 1.elemani state degiskenidir.
-  //? 2.si ise state'i degistirmeye izin veren bir setter metodudur.
+  //?  Acilan dizinin 1.elemani state degiskenidir.
+  //?  2.si ise state'i degistirmeye izin veren bir setter metodudur.
   //? useState parametre olarak state'in ilk degerini alir.
 
   const [count, setCount] = useState(0); //? arr destr.
+
+  const [person, setPerson] = useState({
+    name: "John",
+    surname: "Doe",
+    age: 43,
+  });
 
   const inc = () => {
     setCount(count + 1);
   };
 
-  const dec = () => {
-    if (count > 0) {
-      setCount(count - 1);
-    }
+  // const dec = () => {
+  //   if (count > 0) {
+  //     setCount(count - 1);
+  //   }
+  // };
+
+  const incAge = () => {
+    //?Bu sekilde bir atama ile sayisal deger state'in uzerine yazilmis oldu.
+    //? Dolayisiyla obje yapisi bozuldu.
+    // setPerson(person.age + 1);
+    // setPerson({ name: "Ahmet", surname: "Can", age: 44 });
+
+    setPerson({ ...person, age: person.age + 1 });
   };
+
+  console.log(person);
+
   return (
     <div className="container text-center mt-4">
-      <h1>USESTATE HOOK</h1>
-      <h2 className="display-4 text-danger">COUNT:{count}</h2>
-      <button onClick={inc} className="btn btn-success">
-        INC
-      </button>
-      <button onClick={() => setCount(0)} className="btn btn-dark">
-        CLR
-      </button>
+      <section>
+        <h1>USESTATE HOOK</h1>
+        <h2 className="display-4 text-danger">COUNT:{count}</h2>
+        <button onClick={inc} className="btn btn-success">
+          INC
+        </button>
 
-      <button onClick={dec} className="btn btn-danger">
+        <button onClick={() => setCount(0)} className="btn btn-dark">
+          CLR
+        </button>
+        {/* <button onClick={dec} className="btn btn-danger">
         DEC
-      </button>
+      </button> */}
+        <button
+          onClick={() => count > 0 && setCount(count - 1)}
+          className="btn btn-danger"
+        >
+          DEC
+        </button>
+      </section>
+      <section>
+        <h1>USESTATE OBJECT</h1>
+        <h2>{person.name}</h2>
+        <h2>{person.surname}</h2>
+        <h4>{person.age}</h4>
+        <button onClick={incAge} className="btn btn-info">
+          inc age
+        </button>
+      </section>
     </div>
   );
 };
