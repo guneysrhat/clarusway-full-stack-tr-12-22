725c0951b4fd85f2791f53439e98f607a017a54b
guneysrhat <guneysrhat@gmail.com>
2022-10-13 22:39:57 +0300

    KEYBORD

--- front_end/react/class-notes/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
+++ front_end/react/class-notes/rc06_forms-otherEvents/src/components/2-keyboardClipboardEvents/KeyboardClipboard.js
@@ -1,8 +1,54 @@
+import { useState } from "react";
+
 const KeyboardClipboard = () => {
+  const [inputValue, setInputValue] = useState("");
+
+  const handleKeyDown = (e) => {
+    console.log(e.keyCode);
+    if (e.keyCode >= 48 && e.keyCode <= 57) {
+      alert("Please dont enter a number");
+      e.preventDefault();
+    }
+    e.keyCode === 13 && setInputValue("");
+  };
+
+  const handleAreaPaste = (e) => {
+    e.target.style.border = "2px solid red";
+    e.target.value += e.clipboardData.getData("text").toLocaleUpperCase();
+    e.preventDefault();
+  };
+
   return (
-    <div className="container test-center">
-      <h1>Clipboard Event</h1>
-      KeyboardClipboard
+    <div className="container text-center">
+      <h1>CLIPBOARD EVENTS</h1>
+      <input
+        className="form-control"
+        type="text"
+        value={inputValue}
+        onChange={(e) => {
+          e.target.value = e.target.value.toLocaleUpperCase();
+          setInputValue(e.target.value);
+        }}
+        onKeyDown={handleKeyDown}
+      />
+
+      <p
+        onCopy={(e) => {
+          e.preventDefault();
+          alert("you cannot copy this doc");
+        }}
+        className="text-start mt-4"
+      >
+        {inputValue}
+      </p>
+
+      <textarea
+        className="form-control"
+        name="area"
+        id="area"
+        rows="10"
+        onPaste={handleAreaPaste}
+      ></textarea>
     </div>
   );
 };
