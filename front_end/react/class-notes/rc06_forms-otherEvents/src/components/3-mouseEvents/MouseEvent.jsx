1e8da70f976b0e1e7ae943ff7c342729744b9d6b
guneysrhat <guneysrhat@gmail.com>
2022-10-13 23:04:50 +0300

    mouse

--- front_end/react/class-notes/rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
+++ front_end/react/class-notes/rc06_forms-otherEvents/src/components/3-mouseEvents/MouseEvent.jsx
@@ -1,22 +1,63 @@
+import { useState } from "react";
+
 const MouseEvent = () => {
+  const [showHint, setShowHint] = useState(false);
+  const [toggle, setToggle] = useState(false);
+
+  const [coordX, setCoordX] = useState();
+  const [coordY, setCoordY] = useState();
+
+  const handleDoubleClick = (e) => {
+    setToggle(!toggle);
+    toggle
+      ? (e.target.className = "bg-success text-light w-50 p-4 mt-4")
+      : (e.target.className = "bg-danger text-light w-50 p-4 mt-4");
+  };
+
+  console.log(toggle);
+
+  //? mouseMove event
+  const handleMouseMove = (e) => {
+    setCoordX(e.nativeEvent.offsetX); //! Göreceli
+    setCoordY(e.nativeEvent.offsetY);
+    // setCoordX(e.pageX);
+    // setCoordY(e.pageY);
+  };
+
   return (
     <div className="container text-center d-flex flex-column align-items-center mt-4">
       <h2 className="text-danger">MOUSE EVENTS</h2>
 
-      <div id="todo-1" className="bg-success text-light w-50 p-4">
+      <div
+        id="todo-1"
+        className="bg-success text-light w-50 p-4"
+        onMouseEnter={() => setShowHint(true)}
+        onMouseOut={() => setShowHint(false)}
+      >
         todo item 1
       </div>
+      {showHint && <span>hello React</span>}
 
-      <div id="todo-2" className="bg-success text-light w-50 p-4 mt-4">
+      <div
+        id="todo-2"
+        className="bg-success text-light w-50 p-4 mt-4"
+        onDoubleClick={handleDoubleClick}
+      >
         todo item 2
       </div>
 
-      <div id="todo-3" className="bg-success text-light w-50 p-4 my-4">
+      <div
+        id="todo-3"
+        className="bg-success text-light w-50 p-4 my-4"
+        onMouseMove={handleMouseMove}
+      >
         todo item 3
       </div>
 
       <p>X and Y</p>
-      <p className="text-danger fw-bold">coordX - coordY</p>
+      <p className="text-danger fw-bold">
+        {coordX} {coordY}
+      </p>
     </div>
   );
 };
