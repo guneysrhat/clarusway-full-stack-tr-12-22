2f8773cf4ad12b40c9084e0e036717cfaa345757
guneysrhat <guneysrhat@gmail.com>
2022-10-13 22:41:21 +0300

    MOUSE

--- front_end/react/class-notes/rc06_forms-otherEvents/src/App.js
+++ front_end/react/class-notes/rc06_forms-otherEvents/src/App.js
@@ -1,15 +1,15 @@
 //import Form from "./components/1-forms/Form";
 //import FormObject from "./components/1-forms/FormObject";
-import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
-//import MouseEvent from "./components/3-mouseEvents/MouseEvent";
+//import KeyboardEvents from "./components/2-keyboardClipboardEvents/KeyboardClipboard";
+import MouseEvent from "./components/3-mouseEvents/MouseEvent";
 
 function App() {
   return (
     <div className="container mt-4 text-center">
       {/* <Form /> */}
       {/* <FormObject /> */}
-      <KeyboardEvents />
-      {/* <MouseEvent /> */}
+      {/* <KeyboardEvents /> */}
+      <MouseEvent />
     </div>
   );
 }
