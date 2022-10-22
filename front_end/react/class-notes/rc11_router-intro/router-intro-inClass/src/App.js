945ba6f2e93a40483f7198d513b8d163863cfe86
guneysrhat <guneysrhat@gmail.com>
2022-10-22 11:48:09 +0300

    router

--- front_end/react/class-notes/rc11_router-intro/router-intro-inClass/src/App.js
+++ front_end/react/class-notes/rc11_router-intro/router-intro-inClass/src/App.js
@@ -4,17 +4,20 @@ import Contact from "./pages/Contact";
 import Home from "./pages/Home";
 import Instructors from "./pages/Instructors";
 import NotFound from "./pages/NotFound";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <>
+    <BrowserRouter>
       <Nav />
-      <Home />
-      <Instructors />
-      <Contact />
-      <NotFound />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/instructors" element={<Instructors />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   );
 }
 
