88411ead56e5bfae8b29e10d3c2a27de367bab0d
guneysrhat <guneysrhat@gmail.com>
2022-10-22 13:53:47 +0300

    navigate back

--- front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,11 +1,29 @@
 import { useLocation, useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const PersonDetail = () => {
+  const navigate = useNavigate();
   const { id } = useParams();
   console.log(id);
   const { state: person } = useLocation();
   console.log(person);
-  return <div>PersonDetail</div>;
+  return (
+    <div className="container text-center">
+      <h3>
+        {person.first_name} {person.last_name}
+      </h3>
+      <img className="rounded" src={person.avatar} alt="" />
+      <p>{person.email}</p>
+      <div>
+        <button className="btn btn-success me-2" onClick={() => navigate("/")}>
+          Home
+        </button>
+        <button className="btn btn-warning" onClick={() => navigate(-1)}>
+          Go Back
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default PersonDetail;
