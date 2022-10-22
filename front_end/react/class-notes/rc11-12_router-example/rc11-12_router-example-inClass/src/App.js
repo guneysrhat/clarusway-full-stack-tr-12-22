dd772d6d7dd259288202e281adf2e0b72c5c42d3
guneysrhat <guneysrhat@gmail.com>
2022-10-22 13:21:57 +0300

    person detail

--- front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
+++ front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/App.js
@@ -6,6 +6,7 @@ import People from "./pages/People";
 import NotFound from "./pages/NotFound";
 import { Routes, Route } from "react-router-dom";
 import Paths from "./pages/Paths";
+import PersonDetail from "./pages/PersonDetail";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/people" element={<People />} />
+        <Route path="/people/:id" element={<PersonDetail />} />
         <Route path="/paths" element={<Paths />} />
         <Route path="/contact" element={<Contact />} />
         <Route path="*" element={<NotFound />} />
---
+++ front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -0,0 +1,5 @@
+const PersonDetail = () => {
+  return <div>PersonDetail</div>;
+};
+
+export default PersonDetail;
