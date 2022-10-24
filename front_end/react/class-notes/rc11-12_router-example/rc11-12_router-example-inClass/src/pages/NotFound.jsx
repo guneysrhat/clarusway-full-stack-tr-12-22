e84ae2a4865a1aab7637e4a08e08a6ca0ed80981
guneysrhat <guneysrhat@gmail.com>
2022-10-24 21:26:59 +0300

    error handling

--- front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/NotFound.jsx
+++ front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/NotFound.jsx
@@ -1,12 +1,18 @@
 import errorImg from "../img/404.png";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
+  const navigate = useNavigate();
   return (
     <div className="container text-center mt-4">
       <img className="w-75" src={errorImg} alt="" />
       <div>
-        <button className="btn btn-success me-2">Home</button>
-        <button className="btn btn-warning">Go Back</button>
+        <button onClick={() => navigate("/")} className="btn btn-success me-2">
+          Home
+        </button>
+        <button onClick={() => navigate("-1")} className="btn btn-warning">
+          Go Back
+        </button>
       </div>
     </div>
   );
--- front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
+++ front_end/react/class-notes/rc11-12_router-example/rc11-12_router-example-inClass/src/pages/PersonDetail.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams, useLocation, useNavigate } from "react-router-dom";
+import NotFound from "./NotFound";
 
 const PersonDetail = () => {
   const navigate = useNavigate();
@@ -14,33 +15,46 @@ const PersonDetail = () => {
   // console.log(person);
 
   const [person, setPerson] = useState("");
+  const [error, setError] = useState(false);
 
   useEffect(() => {
     fetch(`https://reqres.in/api/users/${id}`)
-      .then((res) => res.json())
+      .then((res) => {
+        if (!res.ok) {
+          setError(true);
+          throw new Error("Something went wrong");
+        }
+        return res.json();
+      })
       .then((data) => setPerson(data.data))
       .catch((err) => console.log(err));
   }, []);
 
   console.log(person);
-
-  return (
-    <div className="container text-center">
-      <h3>
-        {person?.first_name} {person?.last_name}
-      </h3>
-      <img className="rounded" src={person?.avatar} alt="" />
-      <p>{person?.email}</p>
-      <div>
-        <button onClick={() => navigate("/")} className="btn btn-success me-2">
-          Go Home
-        </button>
-        <button onClick={() => navigate(-1)} className="btn btn-warning">
-          Go Back
-        </button>
+  if (error) {
+    return <NotFound />;
+  } else {
+    return (
+      <div className="container text-center">
+        <h3>
+          {person?.first_name} {person?.last_name}
+        </h3>
+        <img className="rounded" src={person?.avatar} alt="" />
+        <p>{person?.email}</p>
+        <div>
+          <button
+            onClick={() => navigate("/")}
+            className="btn btn-success me-2"
+          >
+            Go Home
+          </button>
+          <button onClick={() => navigate(-1)} className="btn btn-warning">
+            Go Back
+          </button>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default PersonDetail;
