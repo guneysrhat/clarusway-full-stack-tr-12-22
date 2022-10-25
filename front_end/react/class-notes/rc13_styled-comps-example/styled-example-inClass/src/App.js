e02cbf21bd9e35b6519c8baade6c9fcf6f51f3e8
guneysrhat <guneysrhat@gmail.com>
2022-10-25 22:30:35 +0300

    button

--- front_end/react/class-notes/rc13_styled-comps-example/styled-example-inClass/src/App.js
+++ front_end/react/class-notes/rc13_styled-comps-example/styled-example-inClass/src/App.js
@@ -1,7 +1,9 @@
+import Header from "./components/Header";
+
 const App = () => {
   return (
     <>
-      <h1>APP</h1>
+      <Header />
     </>
   );
 };
---
+++ front_end/react/class-notes/rc13_styled-comps-example/styled-example-inClass/src/components/Header.jsx
@@ -0,0 +1,12 @@
+import Button from "./styles/Button.styled";
+
+const Header = () => {
+  return (
+    <div>
+      <Button color="#A62440">Apply Courses</Button>
+      <Button bg="#A62440">Talk to Adviser</Button>
+    </div>
+  );
+};
+
+export default Header;
