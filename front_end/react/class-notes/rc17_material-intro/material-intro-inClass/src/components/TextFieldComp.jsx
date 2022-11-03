8badd8c3a14646b9ef8640b3910f1db3930842f3
guneysrhat <guneysrhat@gmail.com>
2022-11-03 21:26:01 +0300

    create text filed

--- front_end/react/class-notes/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
+++ front_end/react/class-notes/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
@@ -2,9 +2,22 @@ import Typography from "@mui/material/Typography";
 import Container from "@mui/material/Container";
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 const TextFieldComp = () => {
-  return <div>TextFieldComp</div>;
+  return (
+    <div>
+      <Container>
+        <Typography variant="h4" color="error" align="center" mt={4}>
+          TEXT FIELD
+        </Typography>
+
+        <Box>
+          <TextField />
+        </Box>
+      </Container>
+    </div>
+  );
 };
 
 export default TextFieldComp;
