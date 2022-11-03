fac7899cdcfeda015547131ee19941e1ce18f7d3
guneysrhat <guneysrhat@gmail.com>
2022-11-03 22:29:18 +0300

    app bar

--- front_end/react/class-notes/rc17_material-intro/material-intro-inClass/src/pages/Home.jsx
+++ front_end/react/class-notes/rc17_material-intro/material-intro-inClass/src/pages/Home.jsx
@@ -1,10 +1,12 @@
 import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
 import CardGrid from "../components/CardGrid";
+import AppbarComp from "../components/AppbarComp";
 
 const Home = () => {
   return (
     <>
+      <AppbarComp />
       <TypoButtons />
       <TextFieldComp />
       <CardGrid />
