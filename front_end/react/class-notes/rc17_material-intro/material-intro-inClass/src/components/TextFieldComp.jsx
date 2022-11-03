d8b6412f56b38a55787a7b255f13d3f707124f8d
guneysrhat <guneysrhat@gmail.com>
2022-11-03 20:56:54 +0300

    TextFieldComp

---
+++ front_end/react/class-notes/rc17_material-intro/material-intro-inClass/src/components/TextFieldComp.jsx
@@ -0,0 +1,11 @@
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Box from "@mui/material/Box";
+
+
+const TextFieldComp = () => {
+  return <div>TextFieldComp</div>;
+};
+
+export default TextFieldComp;
