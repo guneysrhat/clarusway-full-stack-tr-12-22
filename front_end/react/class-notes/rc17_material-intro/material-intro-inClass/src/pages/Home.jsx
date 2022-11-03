0c42cddc9b49353b9dc82f8d86a4d4ef813a051f
guneysrhat <guneysrhat@gmail.com>
2022-11-03 21:37:19 +0300

    create grid

--- front_end/react/class-notes/rc17_material-intro/material-intro-inClass/src/pages/Home.jsx
+++ front_end/react/class-notes/rc17_material-intro/material-intro-inClass/src/pages/Home.jsx
@@ -1,11 +1,13 @@
 import TextFieldComp from "../components/TextFieldComp";
 import TypoButtons from "../components/TypoButtons";
+import CardGrid from "../components/CardGrid";
 
 const Home = () => {
   return (
     <>
       <TypoButtons />
       <TextFieldComp />
+      <CardGrid />
     </>
   );
 };
