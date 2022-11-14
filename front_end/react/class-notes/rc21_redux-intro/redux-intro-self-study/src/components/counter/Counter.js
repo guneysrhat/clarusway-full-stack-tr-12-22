24a0deebd988450af5fd7eee43dbf5b68ee3a5da
guneysrhat <guneysrhat@gmail.com>
2022-11-14 21:39:05 +0300

    BASIC redux counetr

--- front_end/react/class-notes/rc21_redux-intro/redux-intro-self-study/src/components/counter/Counter.js
+++ front_end/react/class-notes/rc21_redux-intro/redux-intro-self-study/src/components/counter/Counter.js
@@ -1,14 +1,32 @@
+import { useDispatch, useSelector } from "react-redux";
 import "./Counter.css";
 
 const Counter = () => {
+  const dispatch = useDispatch();
+  const counter = useSelector((state) => state.counter);
   return (
     <div className="app">
       <h2 className="counter-header">Counter With Redux</h2>
-      <h1>counter:0</h1>
+      <h1>{counter}</h1>
       <div>
-        <button className="counter-button positive">increase</button>
-        <button className="counter-button zero">reset</button>
-        <button className="counter-button negative">decrease</button>
+        <button
+          className="counter-button positive"
+          onClick={() => dispatch({ type: "INCREMENT" })}
+        >
+          increase
+        </button>
+        <button
+          className="counter-button zero"
+          onClick={() => dispatch({ type: "CLEAR" })}
+        >
+          reset
+        </button>
+        <button
+          className="counter-button negative"
+          onClick={() => dispatch({ type: "DECREMENT" })}
+        >
+          decrease
+        </button>
       </div>
     </div>
   );
