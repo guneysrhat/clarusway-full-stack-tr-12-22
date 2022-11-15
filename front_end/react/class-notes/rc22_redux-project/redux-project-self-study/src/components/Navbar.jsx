e224c5de38c02804da46c0d887a0a1ce2fc39238
guneysrhat <guneysrhat@gmail.com>
2022-11-15 21:18:47 +0300

    navbar

--- front_end/react/class-notes/rc22_redux-project/redux-project-self-study/src/components/Navbar.jsx
+++ front_end/react/class-notes/rc22_redux-project/redux-project-self-study/src/components/Navbar.jsx
@@ -8,6 +8,11 @@ import { useNavigate } from "react-router-dom";
 
 export default function Navbar() {
   const navigate = useNavigate();
+  const user = true;
+
+  const handleLogout = () => {
+    //TODO
+  };
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" color="secondary">
@@ -20,8 +25,16 @@ export default function Navbar() {
           >
             Clarusway News
           </Typography>
-
-          <Button color="inherit">Login</Button>
+          {user && (
+            <Button color="inherit" onClick={handleLogout()}>
+              Logout
+            </Button>
+          )}
+          {!user && (
+            <Button color="inherit" onClick={() => navigate("/login")}>
+              Login
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
     </Box>
