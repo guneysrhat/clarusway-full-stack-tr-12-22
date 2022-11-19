c8eaa18353776263721581d77c810a6d751b42d6
guneysrhat <guneysrhat@gmail.com>
2022-11-19 13:11:25 +0300

    self study

---
+++ front_end/react/class-notes/rc24_formik-yup/formik-yup-self-study/src/pages/Register.jsx
@@ -0,0 +1,69 @@
+import Container from "@mui/material/Container";
+import Typography from "@mui/material/Typography";
+import Avatar from "@mui/material/Avatar";
+import LockIcon from "@mui/icons-material/Lock";
+import image from "../assets/result.svg";
+import Grid from "@mui/material/Grid";
+
+import { Link, useNavigate } from "react-router-dom";
+import { Box } from "@mui/material";
+import { useSelector } from "react-redux";
+
+const Register = () => {
+  const navigate = useNavigate();
+  const { currentUser, error } = useSelector((state) => state.auth);
+
+  return (
+    <Container maxWidth="lg">
+      <Grid
+        container
+        justifyContent="center"
+        direction="row-reverse"
+        rowSpacing={{ sm: 3 }}
+        sx={{
+          height: "100vh",
+          p: 2,
+        }}
+      >
+        <Grid item xs={12}>
+          <Typography variant="h3" color="primary" align="center">
+            STOCK APP
+          </Typography>
+        </Grid>
+
+        <Grid item xs={12} sm={10} md={6}>
+          <Avatar
+            sx={{
+              backgroundColor: "secondary.light",
+              m: "auto",
+              width: 40,
+              height: 40,
+            }}
+          >
+            <LockIcon size="30" />
+          </Avatar>
+          <Typography
+            variant="h4"
+            align="center"
+            mb={2}
+            color="secondary.light"
+          >
+            Register
+          </Typography>
+
+          <Box sx={{ textAlign: "center", mt: 2 }}>
+            <Link to="/">Do you have an account?</Link>
+          </Box>
+        </Grid>
+
+        <Grid item xs={0} sm={7} md={6}>
+          <Container>
+            <img src={image} alt="" />
+          </Container>
+        </Grid>
+      </Grid>
+    </Container>
+  );
+};
+
+export default Register;
