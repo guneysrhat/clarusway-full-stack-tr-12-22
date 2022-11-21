418d2b228f5fc4875d692c759a09f999424cccfa
guneysrhat <guneysrhat@gmail.com>
2022-11-21 20:00:29 +0300

    stock app start

---
+++ front_end/react/class-notes/rc25_stock-app/stock-app-starter/README.md
@@ -0,0 +1,17 @@
+# Redux Toolkit Ornegi
+
+## `Kurulum`
+
+```
+npm install veya yarn
+```
+
+## `Kullanilan Kutuphaneler`
+
+- `@reduxjs/toolkit`
+- `react-redux`
+- `axios`
+- `react-router-dom`
+- `@mui/material-ui`
+- `@emotion/react`
+- `@emotion/styled`
---
+++ front_end/react/class-notes/rc25_stock-app/stock-app/src/pages/Dashboard.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+
+import Toolbar from "@mui/material/Toolbar";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+
+import { useSelector } from "react-redux";
+import useAuthCalls from "../hooks/useAuthCalls";
+
+function Dashboard() {
+  const { currentUser } = useSelector((state) => state.auth);
+  const { logout } = useAuthCalls();
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar position="fixed">
+        <Toolbar>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            STOCK APP
+          </Typography>
+          {currentUser && (
+            <Button color="inherit" onClick={() => logout()}>
+              Logout
+            </Button>
+          )}
+        </Toolbar>
+      </AppBar>
+    </Box>
+  );
+}
+
+export default Dashboard;
